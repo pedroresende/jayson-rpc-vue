155dcd25357c5876c4f043ee9a7442646f91da5f
Andrew Larsson <andrew@developersbliss.com>
2018-09-29 01:23:57 -0600

    Use options object instead of host for versatility

--- index.js
+++ index.js
@@ -3,14 +3,10 @@
 import jayson from 'jayson/lib/client';
 
 export default {
-  install: function(Vue, url = url, ssl = false) {
-    let client = jayson.http({
-      host: url
-    });
+  install: function(Vue, options = options, ssl = false) {
+    let client = jayson.http(options);
     if (ssl) {
-      client = jayson.https({
-        host: url
-      });
+      client = jayson.https(options);
     }
 
     Object.defineProperty(Vue.prototype, '$JaysonRPC', { value: client });
